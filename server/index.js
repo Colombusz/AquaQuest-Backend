2d259ce8fb89554e2dca4c76505a1cfbb1e107dc
ELI <Eliseo.lim12@gmail.com>
2025-02-09 21:28:25 +0800

    mongo

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+import express from "express";
+import { connectDB } from "../config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server listening on ${PORT}`);
 });
