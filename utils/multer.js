c7a4b4a1d2dc66e86cfebb6d025270df3578b3dd
YamiiiN <159107432+YamiiiN@users.noreply.github.com>
2025-02-22 17:46:15 +0800

    multer update

--- utils/multer.js
+++ utils/multer.js
@@ -20,6 +20,40 @@
 // export default upload;
 
 
+// import multer from 'multer';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+
+// // Get directory path for storing uploads
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, path.join(__dirname, '../uploads')); // Save files to 'uploads' folder
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + path.extname(file.originalname)); // Rename file with timestamp
+//     }
+// });
+
+// const upload = multer({
+//     limits: { fileSize: 50 * 1024 * 1024 },
+//     storage: storage,
+//     fileFilter: (req, file, cb) => {
+//         console.log("File received:", file);
+//         let ext = path.extname(file.originalname).toLowerCase();
+//         if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+//             return cb(new Error("Unsupported file type!"), false);
+//         }
+//         cb(null, true);
+//     },
+// });
+
+// export default upload;
+
+
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
